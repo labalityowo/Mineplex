c2e012237f76d47dce095fe9f621176c475fdb2d
Labality <56186498+Labality@users.noreply.github.com>
2021-05-16 11:06:01 +0700

    Update player_stats.sql

--- sqldumps/player_stats.sql
+++ sqldumps/player_stats.sql
@@ -23,9 +23,10 @@ DROP TABLE IF EXISTS `ipinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ipinfo` (
-  `id` int(11) NOT NULL,
-  `ipAddress` text COLLATE latin1_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ipAddress` text COLLATE latin1_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -34,6 +35,7 @@ CREATE TABLE `ipinfo` (
 
 LOCK TABLES `ipinfo` WRITE;
 /*!40000 ALTER TABLE `ipinfo` DISABLE KEYS */;
+INSERT INTO `ipinfo` VALUES (1,'127.0.0.1');
 /*!40000 ALTER TABLE `ipinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -45,10 +47,12 @@ DROP TABLE IF EXISTS `playerinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `playerinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uuid` varchar(100) NOT NULL,
   `name` varchar(40) NOT NULL,
-  `version` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `version` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,6 +61,7 @@ CREATE TABLE `playerinfo` (
 
 LOCK TABLES `playerinfo` WRITE;
 /*!40000 ALTER TABLE `playerinfo` DISABLE KEYS */;
+INSERT INTO `playerinfo` VALUES (1,'0a8cd98e-4b11-4c53-9e34-fc37cbaa7e0c','Labality','47');
 /*!40000 ALTER TABLE `playerinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,6 +85,7 @@ CREATE TABLE `playerips` (
 
 LOCK TABLES `playerips` WRITE;
 /*!40000 ALTER TABLE `playerips` DISABLE KEYS */;
+INSERT INTO `playerips` VALUES (1,1,'2021-05-16'),(1,1,'2021-05-16'),(1,1,'2021-05-16'),(1,1,'2021-05-16');
 /*!40000 ALTER TABLE `playerips` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,9 +97,12 @@ DROP TABLE IF EXISTS `playerloginsessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `playerloginsessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `playerInfoId` int(11) NOT NULL,
-  `loginTime` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `loginTime` text NOT NULL,
+  `timeInGame` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,6 +111,7 @@ CREATE TABLE `playerloginsessions` (
 
 LOCK TABLES `playerloginsessions` WRITE;
 /*!40000 ALTER TABLE `playerloginsessions` DISABLE KEYS */;
+INSERT INTO `playerloginsessions` VALUES (1,1,'2021-05-16 10:59:05',''),(2,1,'2021-05-16 11:01:15',''),(3,1,'2021-05-16 11:03:05','0.5500000000'),(4,1,'2021-05-16 11:03:38','');
 /*!40000 ALTER TABLE `playerloginsessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,6 +134,7 @@ CREATE TABLE `playeruniquelogins` (
 
 LOCK TABLES `playeruniquelogins` WRITE;
 /*!40000 ALTER TABLE `playeruniquelogins` DISABLE KEYS */;
+INSERT INTO `playeruniquelogins` VALUES (1,'2021-05-16'),(1,'2021-05-16'),(1,'2021-05-16'),(1,'2021-05-16');
 /*!40000 ALTER TABLE `playeruniquelogins` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -136,4 +147,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-16 10:39:44
+-- Dump completed on 2021-05-16 11:05:29
